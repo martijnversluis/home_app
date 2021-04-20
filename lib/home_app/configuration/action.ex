d9f307b5579340a48a05ea155a74d50fed50e2d3
Martijn Versluis <martijnversluis@users.noreply.github.com>
2021-04-20 09:40:02 +0200

    Missing file

---
+++ lib/home_app/configuration/action.ex
@@ -0,0 +1,15 @@
+defmodule HomeApp.Configuration.Action do
+  use HomeApp.Configuration.Schema
+
+  schema "" do
+    field(:action, :string)
+    field(:target, :string)
+    field(:config, HomeApp.Configuration.Config, default: %{})
+  end
+
+  def changeset(struct, attributes) do
+    struct
+    |> cast(attributes, [:action, :target, :config])
+    |> validate_required([:action, :target])
+  end
+end
