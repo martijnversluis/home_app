3309f83a21a66fa19905dd587a81dd453f0672ad
Martijn Versluis <martijnversluis@users.noreply.github.com>
2023-02-11 11:01:43 +0100

    Move charge points to devices

--- lib/smoov/driver.ex
+++ lib/smoov/driver.ex
@@ -1,16 +1,15 @@
 defmodule Smoov.Driver do
   use HomeApp.DeviceDriver
 
-  defp get_device_value(
-         %{config: %{charge_points: charge_point_ids}} = _interface,
-         devices,
-         _state
-       )
-       when is_list(devices) do
-    IO.inspect(charge_point_ids, label: "Smoov get value")
-
-    Map.new(charge_point_ids, fn charge_point_id ->
-      {charge_point_id, {:ok, Smoov.Client.get_charge_point(charge_point_id)}}
+  defp get_device_value(_interface, devices, _state) when is_list(devices) do
+    Map.new(devices, fn %{id: id, config: %{id: charge_point_id}} ->
+      {
+        id,
+        {
+          :ok,
+          Smoov.Client.get_charge_point(charge_point_id) |> stringify_keys()
+        }
+      }
     end)
   end
 end
