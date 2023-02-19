f5277c8e0c82ac3c2fa0cca6069301d857563a3a
Martijn Versluis <martijnversluis@users.noreply.github.com>
2023-02-19 19:45:40 +0100

    Add Entsoe client

---
+++ lib/entsoe/client.ex
@@ -0,0 +1,62 @@
+defmodule Entsoe.Client do
+  defstruct security_token: nil
+
+  @api_url "https://web-api.tp.entsoe.eu/api"
+
+  def new(security_token) do
+    %__MODULE__{
+      security_token: security_token
+    }
+  end
+
+  def day_ahead_prices(%__MODULE__{} = client, date) do
+    date_time = Timex.to_datetime(date)
+
+    day_start =
+      date_time
+      |> Timex.set(hour: 0, minute: 0)
+      |> Timex.format!("{YYYY}{0M}{0D}{h24}{m}")
+
+    day_end =
+      date_time
+      |> Timex.set(hour: 23, minute: 0)
+      |> Timex.format!("{YYYY}{0M}{0D}{h24}{m}")
+
+    client
+    |> get(%{
+      documentType: "A44",
+      In_Domain: "10YNL----------L",
+      Out_Domain: "10YNL----------L",
+      periodStart: day_start,
+      periodEnd: day_end
+    })
+  end
+
+  defp get(%__MODULE__{security_token: security_token}, %{} = params) do
+    %HTTPoison.Request{
+      method: "get",
+      url: @api_url,
+      params: Map.put(params, :securityToken, security_token)
+    }
+    |> perform_request()
+  end
+
+  defp perform_request(%HTTPoison.Request{} = request) do
+    request
+    |> HTTPoison.request()
+    |> process_response()
+  end
+
+  defp process_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
+    body
+
+    #    case parse_xml(body) do
+    #      %Entsoe.Document{} = document -> {:ok, document}
+    #      {:error, _} -> {:error, "Parsing XML failed"}
+    #    end
+  end
+
+  defp process_response({:error, %HTTPoison.Error{reason: reason}}) do
+    {:error, reason}
+  end
+end
